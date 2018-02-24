7236d59943627252b0a04b88df24d7de174ef97a
James Tognola <togdem1@googlemail.com>
2018-02-24 21:16:24 +0000

    Added basic client app structure.

---
+++ Seminar/Scripts/app/app.routes.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("app.courses")
+        .run(appRun);
+
+    appRun.$inject = ["routerHelper"];
+
+    function appRun(routerHelper) {
+        routerHelper.configureStates(getStates());
+    }
+
+    function getStates() {
+        return [];
+    }
+})();
